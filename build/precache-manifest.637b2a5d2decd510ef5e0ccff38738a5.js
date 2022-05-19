1141775aae81de44175f91b05232d33c44d6c41e
partha690 <84571185+partha690@users.noreply.github.com>
2022-05-19 23:36:25 +0530

    Add files via upload

---
+++ build/precache-manifest.637b2a5d2decd510ef5e0ccff38738a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd059e3fe84f15be5329108e9ffdfbc",
+    "url": "/my-todo/index.html"
+  },
+  {
+    "revision": "ae82a9605625eec9e934",
+    "url": "/my-todo/static/css/main.7eb8ce57.chunk.css"
+  },
+  {
+    "revision": "8b4d68f98558515c658d",
+    "url": "/my-todo/static/js/2.0181470c.chunk.js"
+  },
+  {
+    "revision": "ae82a9605625eec9e934",
+    "url": "/my-todo/static/js/main.e51e7a9c.chunk.js"
+  },
+  {
+    "revision": "8c1011f264757afa8ac0",
+    "url": "/my-todo/static/js/runtime~main.f98e7811.js"
+  },
+  {
+    "revision": "8798f9f5c76d75118de1c5942ec3bf4c",
+    "url": "/my-todo/static/media/bg.8798f9f5.jpg"
+  }
+]);
